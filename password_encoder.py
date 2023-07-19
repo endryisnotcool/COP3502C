3dc28b7f02d40bcb50117d701dc94d800f110ac2
Nakisha Paulin <npaulin@ufl.edu>
2023-07-19 16:15:49 -0400

    Nakisha Paulin - this should work with the decode I added

--- password_encoder.py
+++ password_encoder.py
@@ -9,7 +9,7 @@ def main():     # Endry Rodriguez
         if menu_option == 1:
             password = input('Please enter a password to encode: ')
             encoded_password = encode(password)
-            print('Your passsword has been encoded and stored!')
+            print('Your password has been encoded and stored!')
             continue
         elif menu_option == 2:
             print(f'The encoded password is {encode(password)}, and the original password is {decode(encoded_password)}.')
@@ -17,16 +17,22 @@ def main():     # Endry Rodriguez
             break
         False
 
+
 def encode(string):
     encode_output = ''
     for num in range(len(string)):
-            x = str((int(string[num]) + 3) % 10)
-            encode_output += x
+        x = str((int(string[num]) + 3) % 10)
+        encode_output += x
     return encode_output
 
-def decode():
-    pass
+
+def decode(new_string):  # Nakisha Paulin
+    decode_output = ''
+    for num in range(len(new_string)):
+        y = str((int(new_string[num]) - 3) % 10)  # Reverse of encode output to get original password
+        decode_output += y
+    return decode_output
 
 
-if __name__ == " __main__":
+if __name__ == "__main__":
     main()
