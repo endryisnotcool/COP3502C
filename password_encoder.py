6bd05b3c0f93af31b4b49d60f36a887935ae52b4
Endry Rodriguez <139926363+endryisnotcool@users.noreply.github.com>
2023-07-18 22:58:08 -0400

    password_encoder.py
    I just forgot to add my main() function and my name.

--- password_encoder.py
+++ password_encoder.py
@@ -13,20 +13,24 @@ def encode(string):
             break
     return encode_output
 
-while True:
-    print('Menu')
-    print('-------------')
-    print('1. Encode')
-    print('2. Decode')
-    print('3. Quit')
-    menu_option = int(input('Please enter an option: '))
-    if menu_option == 1:
-        password = input('Please enter a password to encode: ')
-        encoded_password = encode(password)
-        print('Your passsword has been encoded and stored!')
-        continue
-    elif menu_option == 2:
-        print(f'The encoded password is {encoded_password}, and the original password is {password}.')
-    elif menu_option == 3:
-        break
-    False
+def main():    # Endry Rodriguez
+    while True:
+        print('Menu')
+        print('-------------')
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit')
+        menu_option = int(input('Please enter an option: '))
+        if menu_option == 1:
+            password = input('Please enter a password to encode: ')
+            encoded_password = encode(password)
+            print('Your passsword has been encoded and stored!')
+            continue
+        elif menu_option == 2:
+            print(f'The encoded password is {encoded_password}, and the original password is {password}.')
+        elif menu_option == 3:
+            break
+        False
+
+if __name__ == " __main__":
+    main()
